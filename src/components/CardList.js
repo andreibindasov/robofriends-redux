cd0b7c8b052399e218b42caa1ab38c9910d845bd
andreibindasov <>
2021-03-30 08:36:41 -0600

    Update CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
   console.log("CardList");
+
+  const [cardList, setCardList] = useState(0)
+  const cards = useRef("Hello")
+  
+  useEffect(()=>{
+    cards.current=cards.current + '!'
+  }, [cardList])
+
   return (
+    
     <div>
+      <button onClick={()=>setCardList(cardList+1)}>Click</button>
+      <h2>{cardList}</h2>
+      <h3>{cards.current} HEY</h3>
       {robots.map((user, i) => {
         return (
           <Card
